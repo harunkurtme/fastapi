dc79e5cc1cb178e36d989b4fc8a3f0a34be01097
harunkurt.me <harunlakodla@gmail.com>
2022-09-09 15:57:29 +0300

    add booksapp new function post method , new added a book title and author

--- bookapp/books.py
+++ bookapp/books.py
@@ -70,3 +70,16 @@ async def optinalCallBook(skip_book:Optional[str]=None):
         del new_books[skip_book]
         return new_books
     return BOOKS
+
+@app.post("/")
+async def create_book(book_title,book_author):
+    current_book_id=0
+    if len(BOOKS)>0:
+        for book in BOOKS:
+            x= int(book.split('_')[-1])
+            if x>current_book_id:
+                current_book_id=x
+                
+    BOOKS[f'book_{current_book_id+1}']={'title':book_title,'author':book_author}
+    
+    return BOOKS[f'book_{current_book_id+1}']
