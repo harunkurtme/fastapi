5586b2fff952d5eb5da9ff5190b6726a52d21265
harunkurt.me <harunlakodla@gmail.com>
2022-09-09 16:49:31 +0300

    add classes4books.py inside class and new method created from fastapi post with get

--- bookapp/classes4books.py
+++ bookapp/classes4books.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from uuid import UUID
 
 """
 _summary_
@@ -11,6 +13,14 @@ delete / book uuid specific book
 we create BASEMODEL with class
 """
 
+class Book(BaseModel):
+    id: UUID
+    title:str
+    author: str
+    description:str
+    rating:int
+    
+
 app =FastAPI()
 BOOKS=[]
 
@@ -18,3 +28,8 @@ BOOKS=[]
 async def read_all_books():
     return BOOKS
 
+
+@app.post('/')
+async def create_book(book:Book):
+    BOOKS.append(book)
+    return book
