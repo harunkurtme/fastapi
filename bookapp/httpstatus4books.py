b85b4537f4beae3c145b7bbd8acf1a56c22499a8
harunkurt.me <harunlakodla@gmail.com>
2022-09-10 05:49:18 +0300

    add new file httpstatus.py and added new method with post login page

--- bookapp/httpstatus4books.py
+++ bookapp/httpstatus4books.py
@@ -1,7 +1,7 @@
 from email import header
 # from http.client import 
 from urllib.request import Request
-from fastapi import FastAPI,HTTPException,status
+from fastapi import FastAPI,HTTPException,status,Form
 from pydantic import BaseModel,Field
 from uuid import UUID
 
@@ -112,6 +112,11 @@ async def negative_number_exception_handler(request:Request,
         }
     )
     pass
+
+
+@app.post("/book/login")
+async def read_book(username:str=Form(),password:str=Form()):
+    return {"username":username,"password":password}
 
 @app.get("/")
 async def read_all_books(books_to_return: Optional[int]=None):
