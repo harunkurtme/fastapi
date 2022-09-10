e879697fef56c9672402baec530f93309e7c5ad0
harunkurt.me <harunlakodla@gmail.com>
2022-09-10 22:21:51 +0300

    add new file for todoapp add new function read_todo

--- todoapp/main.py
+++ todoapp/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI, Depends
+from http.client import HTTPException
+from pyexpat import model
+from fastapi import FastAPI, Depends,HTTPException
 import models
 from database import engine,sessionlocal
 from sqlalchemy.orm import Session
@@ -22,4 +24,12 @@ async def create_database(db:Session=Depends(get_db)):
     return db.query(models.Todos).all()
 
 
-@app.get
+@app.get("/todo/{todo_id}")
+async def read_todo(todo_id:int,db:Session=Depends(get_db)):
+    todo_model=db.query(models.Todos).filter(models.Todos.id==todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    raise http_exception()
+
+def http_exception():
+    return HTTPException(status_code=404,detail="Todo not found")
