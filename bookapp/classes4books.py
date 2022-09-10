58718086711c569ee2e7a28227e8dd1808acb08a
harunkurt.me <harunlakodla@gmail.com>
2022-09-10 04:17:49 +0300

    classes4book file added a method and read_book id

--- bookapp/classes4books.py
+++ bookapp/classes4books.py
@@ -17,6 +17,10 @@ Optinal library null saffety with field
 
 """
 
+app =FastAPI()
+BOOKS=[]
+
+
 class Book(BaseModel):
     id: UUID
     title:str = Field(min_length=1) #must be one character for title
@@ -46,13 +50,59 @@ class Book(BaseModel):
             }
         }
 
-app =FastAPI()
-BOOKS=[]
+def create_books_no_api():
+    book_1 = Book(id="71f4c2ea-1340-41f4-89f7-2852347bb0d1",
+                  title="Title 1",
+                  author="Author 1",
+                  description="Description 1",
+                  rating=60)
+    book_2 = Book(id="21f4c2ea-1340-41f4-89f7-2852347bb0d1",
+                  title="Title 2",
+                  author="Author 2",
+                  description="Description 2",
+                  rating=70)
+    book_3 = Book(id="31f4c2ea-1340-41f4-89f7-2852347bb0d1",
+                  title="Title 3",
+                  author="Author 3",
+                  description="Description 3",
+                  rating=80)
+    book_4 = Book(id="41f4c2ea-1340-41f4-89f7-2852347bb0d1",
+                  title="Title 4",
+                  author="Author 4",
+                  description="Description 4",
+                  rating=90)
+    BOOKS.append(book_1)
+    BOOKS.append(book_2)
+    BOOKS.append(book_3)
+    BOOKS.append(book_4)
+
+
+
 
 @app.get("/")
-async def read_all_books():
+async def read_all_books(books_to_return: Optional[int]=None):
+
+    # this is book length not make 
+    # maked a lots books
+    if len(BOOKS)<1:
+        create_books_no_api()
+    
+    
+    if books_to_return and len(BOOKS)>=books_to_return >0:
+        i =1
+        new_books=[]
+        while i <=books_to_return:
+            new_books.append(BOOKS[i-1])
+            i+=1
+        return new_books
     return BOOKS
 
+
+@app.get("/book/{book_id}")
+async def read_book(book_id:UUID):
+    for x in BOOKS:
+        if x.id==book_id:
+            
 
 @app.post('/')
 async def create_book(book:Book):
