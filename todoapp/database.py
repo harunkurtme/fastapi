38fe0e62f9fe94ac2f9d55185e238f06312c752e
harunkurt.me <harunlakodla@gmail.com>
2022-09-10 18:23:06 +0300

    add new file for todoapp model

--- todoapp/database.py
+++ todoapp/database.py
@@ -1,8 +1,15 @@
+from sqlite3 import connect
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
 database_URL="sqlite:///./todo.db"
 
 
 engine =create_engine(
-    database_URL,
+    database_URL,connect_args={
+        "check_same_thread":False
+    }
 )
+
+sessionlocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
